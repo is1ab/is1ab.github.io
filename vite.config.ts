92c977b0e18bab3763abe7b287d52a023289e060
黃漢軒 <t109590031@ntut.org.tw>
2024-06-11 23:45:26 +0800

    Fix: DefineConfig

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/is1ab-Website/",
+  base: "/Is1ab-website/",
   plugins: [react()],
 })
