2a049b2201d2a5ed0e9c1363fc59696e1f029432
黃漢軒 <t109590031@ntut.org.tw>
2024-06-11 23:55:19 +0800

    Fix: Wrong href

--- src/component/Introduction.tsx
+++ src/component/Introduction.tsx
@@ -1,8 +1,11 @@
+import { useNavigate } from "react-router-dom"
+
 export function Introduction(){
+    const navigate = useNavigate()
     return (
         <div className="p-3 py-4 introduction">
             <h4 className="text-center fw-bold mb-3">簡介</h4>
-            <p> <span style={{fontWeight: 800}}>Information Security Laboratory</span> (ISLab@NTUT) was founded in August 2022 in the Department of Computert Science and Information Engineering, National Taipei University of Technology. Led by professor <a href="/about-prof">Chin-Yu Sun</a>, our team aims to design original methodologies and develop practical security systems to meet the different demands of clients.</p>
+            <p> <span style={{fontWeight: 800}}>Information Security Laboratory</span> (ISLab@NTUT) was founded in August 2022 in the Department of Computert Science and Information Engineering, National Taipei University of Technology. Led by professor <a className="text-decoration-underline" onClick={() => navigate("/about_prof")} style={{cursor: 'pointer'}}>Chin-Yu Sun</a>, our team aims to design original methodologies and develop practical security systems to meet the different demands of clients.</p>
             <p> Our research topics include: </p>
             <ul style={{lineHeight: 2}}>
                 <li className="text-white"><p><span className="bold-red">Cryptography</span>: <span className="fst-italic">Encryption/Decription, Digital signatures, Authentication schemes, and Privacy issues.</span></p></li>
