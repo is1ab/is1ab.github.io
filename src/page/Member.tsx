1ba3cfb92b06a1ce4ed9f488a31fc1b4ddfd19d8
Yi-ping <alen0421@gmail.com>
2024-07-09 13:49:54 +0800

    add Huang Yi-Ping's personal page

--- src/page/Member.tsx
+++ src/page/Member.tsx
@@ -149,7 +149,8 @@ export function Member(){
             role: "碩零，資安碩士",
             coAdvisor: '',
             experience: "",
-            certificate: ""
+            certificate: "",
+            website: "/#/Member/2024/ypp"
         }
     ]
     const consultantUserTableItem = [
