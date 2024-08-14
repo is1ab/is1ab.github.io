f7d5bc3ab859dd80f0088666870d4252a7db0e9e
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2024-08-14 08:46:38 +0800

    Update Courses.tsx

--- src/page/Courses.tsx
+++ src/page/Courses.tsx
@@ -1,17 +1,51 @@
 export function Course(){
     return (
         <div className="course">
-            <>
-                <h4 className="text-center fw-bold">演講 Speech</h4>
+             <>
+                <h4 className="text-center fw-bold">物件導向程式設計</h4>
                 <ul>
-                    <li>ChatGPT與教學：科技結合的新思維與應用 (北科大學電資學院-教師經驗分享座談會2023.04.18)</li>
+                    <li>113學年度 上學期 [進行中]</li>
+                    <li>112學年度 上學期 [教學評鑑：4.37/5]</li>
+                </ul>
+            </>
+            <>
+                <h4 className="text-center fw-bold">物件導向程式設計實習</h4>
+                <ul>
+                    <li>112學年度 下學期 [教學評鑑：4.52/5]</li>
+                </ul>
+            </>
+            <>
+                <h4 className="text-center fw-bold">安全程式設計</h4>
+                <ul>
+                    <li>112學年度 下學期 [教學評鑑：4.65/5]</li>
+                    <li>111學年度 下學期（大學部） [教學評鑑：4.68/5]</li>
+                    <li>111學年度 下學期（研究所） [教學評鑑：4.82/5]</li>
                 </ul>
             </>
             <hr></hr>
             <>
-                <h4 className="text-center fw-bold">其他 Others</h4>
+                <h4 className="text-center fw-bold">作業系統</h4>
                 <ul>
-                    <li>國語日報 邀訪ChatGPT專題 (2023.04.21)</li>
+                    <li>112學年度 上學期 [教學評鑑：4.64/5]</li>
+                    <li>111學年度 上學期 [教學評鑑：4.94/5]</li>
+                </ul>
+            </>
+            <hr></hr>
+            <>
+                <h4 className="text-center fw-bold">網路安全</h4>
+                <ul>
+                    <li>111學年度 上學期 [教學評鑑：4.64/5]</li>
+                </ul>
+            </>
+            <hr></hr>
+            <>
+                <h4 className="text-center fw-bold">其它課程</h4>
+                <ul>
+                    <li>實務專題(二) 112學年度 上學期 [教學評鑑：N/A]</li>
+                    <li>專題討論 112學年度 下學期 [教學評鑑：N/A]</li>
+                    <li>大學入門與工程倫理 112學年度 上學期 [教學評鑑：N/A]</li>
+                    <li>專題討論 112學年度 上學期 [教學評鑑：N/A]</li>
+                    <li>實務專題(三) 112學年度 上學期 [教學評鑑：N/A]</li>
                 </ul>
             </>
         </div>
