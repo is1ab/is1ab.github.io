d5e8ee1ae7adf187e36d319887e41291f717d582
ntut-xuan <t109590031@ntut.org.tw>
2025-03-05 11:01:39 +0800

    Revert: Format

--- src/main.tsx
+++ src/main.tsx
@@ -86,10 +86,7 @@ const router = createHashRouter(
       <Route path="/Member/2022/Cliff" element={<CliffMemberPage />}></Route>
       <Route path="/Member/2025/JunAn" element={<JunAnMemberPage />}></Route>
       <Route path="/Member/2025/Yueric" element={<YuericMemberPage />}></Route>
-      <Route
-        path="/Member/2024/RobertLiao"
-        element={<RobertLiaoMemberPage />}
-      ></Route>
+      <Route path="/Member/2024/RobertLiao" element={<RobertLiaoMemberPage />}></Route>
       <Route path="/Alumni" element={<Alumni></Alumni>}></Route>
       <Route path="/Partner" element={<Partner></Partner>}></Route>
       <Route path="/Profile" element={<Profile></Profile>}></Route>
