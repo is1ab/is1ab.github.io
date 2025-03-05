b6244ab2fa7d34664862c56c9f4d6d21d1ec9d26
ntut-xuan <t109590031@ntut.org.tw>
2025-03-05 11:06:01 +0800

    Add: Course Link

--- src/page/Courses.tsx
+++ src/page/Courses.tsx
@@ -1,66 +1,69 @@
-export function Course(){
-    return (
-        <div className="course">
-            <>
-                <h4 className="text-center fw-bold">113學年 第2學期</h4>
-                <ul>
-                    <li>安全程式設計 [教學評鑑：Pending...]</li>
-                    <li>物件導向程式設計實習 [教學評鑑：Pending...]</li>
-                </ul>
-            </>
-            
-            <>
-                <h4 className="text-center fw-bold">113學年 第1學期</h4>
-                <ul>
-                    <li>作業系統 [教學評鑑：4.6/5]</li>
-                    <li>物件導向程式設計 [教學評鑑：4.43/5]</li>
-                </ul>
-            </>
+export function Course() {
+  return (
+    <div className="course">
+      <>
+        <h4 className="text-center fw-bold">113學年 第2學期</h4>
+        <ul>
+          <li>
+            <a href="#/Course/2025/SP">安全程式設計 [教學評鑑：Pending...]</a>
+          </li>
+          <li>物件導向程式設計實習 [教學評鑑：Pending...]</li>
+        </ul>
+      </>
 
-            <>
-                <h4 className="text-center fw-bold">112學年 暑假</h4>
-                <ul>
-                    <li>程式設計能力強化研習營-Python程式設計</li>
-                </ul>
-            </>
-            
-            <>
-                <h4 className="text-center fw-bold">112學年 第2學期</h4>
-                <ul>
-                    <li>安全程式設計 [教學評鑑：4.65/5]</li>
-                    <li>物件導向程式設計實習 [教學評鑑：4.52/5]</li>
-                    <li>實務專題(二) [教學評鑑：N/A]</li>
-                    <li>專題討論 [教學評鑑：N/A]</li>
-                </ul>
-            </>
+      <>
+        <h4 className="text-center fw-bold">113學年 第1學期</h4>
+        <ul>
+          <li>作業系統 [教學評鑑：4.6/5]</li>
+          <li>物件導向程式設計 [教學評鑑：4.43/5]</li>
+        </ul>
+      </>
 
-            <>
-                <h4 className="text-center fw-bold">112學年 第1學期</h4>
-                <ul>
-                    <li>作業系統 [教學評鑑：4.64/5]</li>
-                    <li>物件導向程式設計 [教學評鑑：4.37/5]</li>
-                    <li>大學入門與工程倫理 [教學評鑑：N/A]</li>
-                    <li>專題討論 [教學評鑑：N/A]</li>
-                    <li>實務專題(三) [教學評鑑：N/A]</li>
-                </ul>
-            </>
+      <>
+        <h4 className="text-center fw-bold">112學年 暑假</h4>
+        <ul>
+          <li>程式設計能力強化研習營-Python程式設計</li>
+        </ul>
+      </>
 
-            <>
-                <h4 className="text-center fw-bold">111學年 第2學期</h4>
-                <ul>
-                    <li>安全程式設計（大學部） [教學評鑑：4.68/5]</li>
-                    <li>安全程式設計（研究所） [教學評鑑：4.82/5]</li>
-                </ul>
-            </>
-            
-            <>
-                <h4 className="text-center fw-bold">111學年 第1學期</h4>
-                <ul>
-                    <li>作業系統 [教學評鑑：4.94/5]</li>
-                    <li>網路安全 [教學評鑑：4.64/5]</li>
-                </ul>
-            </>
-           
-        </div>
-    )
+      <>
+        <h4 className="text-center fw-bold">112學年 第2學期</h4>
+        <ul>
+          <li>
+            <a href="#/Course/2024/SP">安全程式設計 [教學評鑑：4.65/5]</a>
+          </li>
+          <li>物件導向程式設計實習 [教學評鑑：4.52/5]</li>
+          <li>實務專題(二) [教學評鑑：N/A]</li>
+          <li>專題討論 [教學評鑑：N/A]</li>
+        </ul>
+      </>
+
+      <>
+        <h4 className="text-center fw-bold">112學年 第1學期</h4>
+        <ul>
+          <li>作業系統 [教學評鑑：4.64/5]</li>
+          <li>物件導向程式設計 [教學評鑑：4.37/5]</li>
+          <li>大學入門與工程倫理 [教學評鑑：N/A]</li>
+          <li>專題討論 [教學評鑑：N/A]</li>
+          <li>實務專題(三) [教學評鑑：N/A]</li>
+        </ul>
+      </>
+
+      <>
+        <h4 className="text-center fw-bold">111學年 第2學期</h4>
+        <ul>
+          <li>安全程式設計（大學部） [教學評鑑：4.68/5]</li>
+          <li>安全程式設計（研究所） [教學評鑑：4.82/5]</li>
+        </ul>
+      </>
+
+      <>
+        <h4 className="text-center fw-bold">111學年 第1學期</h4>
+        <ul>
+          <li>作業系統 [教學評鑑：4.94/5]</li>
+          <li>網路安全 [教學評鑑：4.64/5]</li>
+        </ul>
+      </>
+    </div>
+  );
 }
