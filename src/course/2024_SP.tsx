c0da74917fecf8b3fb28d0e36be7eb29810d35c3
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2025-03-07 11:44:41 +0800

    Update 2024_SP.tsx

--- src/course/2024_SP.tsx
+++ src/course/2024_SP.tsx
@@ -37,15 +37,18 @@ export function SP_2024() {
         <li>[Book] Secure Coding in C and C++</li>
         <li>[Book] Secure Programming Cookbook for C and C++</li>
       </div>
-      
+
       <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
-        <h5 className="fw-bold text-white m-0">IPAS 證照通過率</h5>
+        <h5 className="fw-bold text-white m-0">學期成績</h5>
       </div>
       
       <div className="py-3 d-flex flex-column gap-3">
-        <li>[Pending]</li>
+        <li>修課人數: 53 人</li>
+        <li>及格人數: 49 人</li>
+        <li>最高分數: 100 分; 最低分數: 18 分; 平均分數: 89.11 分</li>
+        <li>IPAS 證照通過率: 19/22 = 86 %</li>
       </div>
-      
+            
       <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
         <h5 className="fw-bold text-white m-0">教學評鑑</h5>
       </div>
