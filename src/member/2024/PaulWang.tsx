c060e4413fde6bb6bf46dc0c83a52defe8fa8755
Paul Wang <lswang1112@gmail.com>
2025-03-19 14:06:08 +0800

    修改 Paul Wang 的頁面 - 修改個人簡介 - 新增 picoCTF 比賽經歷

--- src/member/2024/PaulWang.tsx
+++ src/member/2024/PaulWang.tsx
@@ -3,13 +3,26 @@ import PaulWang from "../../assets/member/PaulWang.jpg"
 import { MemberArticleType, MemberCertificateType, MemberInternType, MemberParticipateType, MemberCompetitionExperienceType } from "../../type/MemberCertificateType"
 
 export const PaulWangMemberPage = () => {
-    const bio = "大家好，我是王立翔，研究逆向工程，最近對於行動安全蠻有興趣的，請大家多多指教！"
+    // const bio = "<br>hello"
+    const bio = <div className="d-flex flex-column gap-3">
+                    <p>大家好，我是王立翔，在 IsLab 中參與攻防相關的學術論文研究、也研究逆向工程相關技術、CTF 題目等，歡迎有興趣可以找我討論！</p>
+                    <p>不定期會將 Crackmes、CTF 比賽的結果寫成 Writeups</p>
+                    
+                    <p>- <a href="https://www.notion.so/Paul-s-CTF-writeups-1bb9074f33df80e78ba9ee4abfd64528" target="_blank" rel="noopener noreferrer">我的一般 CTF</a></p>
+                    <p>- <a href="https://github.com/paulwang19/ctf-writeups" target="_blank" rel="noopener noreferrer">我的競賽 CTF</a></p>
+                    <p>- <a href="https://github.com/paulwang19/crackmes-writeups" target="_blank" rel="noopener noreferrer">我的 Crackmes</a></p>
+                </div>
     const certificates: MemberCertificateType[] = []
     const competitionExperiences: MemberCompetitionExperienceType[] = [
         {
             title: "TSCCTF 2025",
             result: "排名: 137/509",
             datetime: "2025/01/13-2025/01/16"
+        },
+        {
+            title: "picoCTF 2025",
+            result: "隊伍排名: 1287/10460 (Team: NTUT_Is1ab)",
+            datetime: "2025/03/08-2025/03/18"
         }
     ]
     const participates: MemberParticipateType[] = [
