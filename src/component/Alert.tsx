15b6aa6aaca3fa12954eb01787389e14bb8546f4
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2025-06-25 20:57:39 +0800

    Update Alert.tsx

--- src/component/Alert.tsx
+++ src/component/Alert.tsx
@@ -1,8 +1,7 @@
 export function Alert(){
     return (
         <div className="p-3 bg-black border border-white border-start-0 border-end-0 border-2 text-center">
-            <p className="fw-bold m-0" style={{color: "yellow"}}>恭喜 黃政、劉硯皓、張庭偉 榮獲 2024 北科實務專題競賽 佳作</p>
-            <p className="fw-bold m-0" style={{color: "yellow"}}>恭喜 吳奕萱、何穎宣、程邦博 榮獲 2024 北科實務專題競賽 佳作</p>
+            <p className="fw-bold m-0" style={{color: "yellow"}}>恭喜 張意昌、吳耀東、林品緯、張睿恩 通過 114 國科會大專生專題研究計畫</p>
         </div>
     )
 }
