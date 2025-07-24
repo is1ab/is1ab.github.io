02daeb5f2853bc100c5e2fe72bc421b32572dc31
lienliensee <152852771+lienliensee@users.noreply.github.com>
2025-07-24 16:10:00 +0800

    Ting Personal Info (#114)
    * Add personal page
    
    * Update package.json
    
    revert vite
    
    * fix: package-lock.json
    
    ---------
    
    Co-authored-by: ting.lien <lienliensee1264@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -51,6 +51,7 @@ import { YuericMemberPage } from "./member/2025/Yueric.tsx";
 import { RobertLiaoMemberPage } from "./member/2024/RobertLiao.tsx";
 import { WillyMemberPage } from "./member/2025/Willy.tsx";
 import { MarcoMemberPage } from "./member/2024/Marco.tsx";
+import { TingMemberPage } from "./member/2025/Ting.tsx";
 
 const router = createHashRouter(
   createRoutesFromElements(
@@ -91,6 +92,7 @@ const router = createHashRouter(
       <Route path="/Member/2024/RobertLiao" element={<RobertLiaoMemberPage />}></Route>
       <Route path="/Member/2025/Willy" element={<WillyMemberPage />}></Route>
       <Route path="/Member/2024/Marco" element={<MarcoMemberPage />}></Route>
+      <Route path="/Member/2025/Ting" element={<TingMemberPage />}></Route>
       <Route path="/Alumni" element={<Alumni></Alumni>}></Route>
       <Route path="/Partner" element={<Partner></Partner>}></Route>
       <Route path="/Profile" element={<Profile></Profile>}></Route>
