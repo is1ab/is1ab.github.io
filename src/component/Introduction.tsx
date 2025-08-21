861f1fc8b57562e56ff0bf48e8cce1950d84b89f
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2025-08-21 20:11:36 +0800

    Update Introduction.tsx

--- src/component/Introduction.tsx
+++ src/component/Introduction.tsx
@@ -8,7 +8,7 @@ export function Introduction(){
         <div className="p-3 py-4 introduction">
             <h4 className="text-center fw-bold mb-3">簡介</h4>
             <p> 國立臺北科技大學資訊工程系<span style={{fontWeight: 800}}>資訊安全實驗室</span> (is1ab@NTUT) 於 2022 年 8 月成立，. 由 <a className="text-decoration-underline" onClick={() => navigate("/Profile")} style={{cursor: 'pointer'}}>孫勤昱</a>教授領導, our team aims to design original methodologies and develop practical security systems to meet the different demands of clients.</p>
-            <p> Our research topics include: </p>
+            <p> 我們的研究主題涵蓋以下方向： </p>
             
             {topics.map((topic) => <Topics topic={topic.title} description={topic.description}></Topics>)}
             
