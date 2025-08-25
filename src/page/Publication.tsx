bb3c72bad103cad34d90e743632beb594f463151
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2025-08-25 22:29:10 +0800

    Update Publication.tsx

--- src/page/Publication.tsx
+++ src/page/Publication.tsx
@@ -40,10 +40,11 @@ export function Publication(){
                 <ol>
                     <li>
                         <Author>
-                            Sheng-Shan Chen, Tun-Wen Pai, and <Professor coAuther/>
+                            Sheng-Shan Chen, Tun-Wen Pai, and <Professor/>
                         </Author>
                         <Title title="EnhanceCTI: An Enhanced Semantic Filtering and Feature Extraction Framework for Industry-Specific Cyber Threat Intelligence"/>
                         Accepted by <Journal name="Computers & Security"/>, August 2025.
+                        <SCI/>
                     </li>
                     <li>
                         <Author>
