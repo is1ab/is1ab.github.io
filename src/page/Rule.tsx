d75cf046d319f89fd62e54010825ff3ffa49478d
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2025-12-01 21:00:10 +0800

    Update Rule.tsx

--- src/page/Rule.tsx
+++ src/page/Rule.tsx
@@ -3,55 +3,68 @@ export function Rule(){
         <div className="rule">
             <h4 className="text-center fw-bold">實驗室組成</h4>
             <ul>
-                <li>碩士班</li>
+                <li> 碩士班 </li>
                     <ul>
-                        <li> CTF 選手, 4 位 </li>
+                        <li> CTF 選手, 6 位 </li>
                         <li> Sunbird 計畫 - 測試工程師, 1 位 </li>
-                        <li> 萬芳醫院計畫 - Prompt injection, 1 位 </li>
-                        <li> 其他計畫, 1 位 </li>
+                    </ul>
+                <li> 畢業題目與研究計畫案有關 </li>
+                    <ul>
+                        <li> 國科會計畫 - 網路安全相關 </li>
+                        <li> 資安院計畫 - 情資、AI相關 </li>
+                        <li> 萬芳醫院計畫 - Prompt injection、LLM Protection </li>
+                        <li> TACC 計畫 - 衛星安全、密碼學相關 </li>
+                        <li> 潛在計畫（資策會）- 金融產業 去識別化技術 </li>
+                        <li> 潛在計畫（公司）- 自動化滲透測試 </li>
                     </ul>
                 <li>大學部</li>
                     <ul>
-                        <li> 專題 - 網路攻防相關, 1組 </li>
-                        <li> 專題 - Sunbird計畫, 1組 </li>
+                        <li> 專題 - 網路攻防相關, 1 組 </li>
+                        <li> 專題 - Sunbird計畫, 1 組 </li>
                     </ul>
             </ul>
             <hr></hr>
             
-            <h4 className="text-center fw-bold"> CTF 研究生義務</h4>
+            <h4 className="text-center fw-bold"> 研究生義務 </h4>
             <ul>
-                <li>入學前</li>
+                <li> 入學前 </li>
                     <ul>
                         <li> 暑假期間需參加 CTF 新生訓練 </li>
                         <li> 選定 CTF 攻防組別 </li>
-                        <li> 參與北科資安社團 is1ab club </li>
+                        <li> 參與北科資安社團 is1ab club (參與攻防組內部討論會) </li>
                         <li> 每週（四）需線上參加 Group meeting </li>
                     </ul>
                 
-                <li>一年級</li>
+                <li> 一年級 </li>
                     <ul>
                         <li> 擔任實驗室相關負責職務 </li>
-                        <li> 餐與攻防組內部討論 </li>
+                        <li> 選定「研究計畫案」主題 </li>
                         <li> 每週（四）需實體參加 Group meeting </li>
                         <li> 參與國內外相關 CTF 比賽 </li>
-                        <li> 參與北科資安社團 is1ab club 活動 </li>
-                        <li> 11-12 月需協助撰寫國科會計畫 </li>
+                        <li> 參與北科資安社團 is1ab club (主持攻防組內部討論會) </li>
+                        <li> 11-12月 需協助撰寫國科會計畫 </li>
                     </ul>
                 
-                <li>一年級暑假</li>
+                <li> 一年級暑假 </li>
                     <ul>
                         <li> 參加 AIS3 活動 </li>
                         <li> 舉辦北聯大 CTF 比賽 </li>
+                        <li> 協助完成北科資安社團相關 KPI </li>
                     </ul>
                 
-                <li>二年級</li>
+                <li> 二年級 </li>
                     <ul>
-                        <li> 8-9月 傳承實驗室相關負責職務 </li>
-                        <li> 11-12 月需撰寫國科會計畫 </li>
-                        <li> 每周（一）需實體參加碩論進度報告 </li>
                         <li> 每週（四）需實體參加 Group meeting </li>
-                        <li> 隔年 4 月口試 </li>
-                        <li> 隔年 5-7 月期間完成「投稿」動作 </li>
+                        <li> 8-9月 傳承實驗室相關負責職務 </li>
+                        <li> 11-12月 需撰寫國科會計畫 </li>
+                        <li> 12月前 研究計畫案 成果報告 </li>
+                        <li> 12-3月 撰寫碩士論文 </li>
+                        <li> 隔年 4月中旬 進行碩士口試 </li>
+                        <li> 隔年 5-8月期間 完成「投稿」 </li>
+                        <li> 隔年 8月底前 完成「離校」 </li>
+                    </ul>
+                    <ul>
+                        <li> 若提前完成研究者，可參與資安企業實習 </li>
                     </ul>
             </ul>
             <hr></hr>
@@ -61,20 +74,10 @@ export function Rule(){
                 <li>學術倫理</li>
                 <li>誠信（嚴禁考試作弊、作業抄襲、論文報告造假...等）</li>
                 <li>實驗室相關安全與衛生規範（用電安全、環境安全、整潔）</li>
-                <li>實驗室參與度*</li>
+                <li>實驗室參與度（團隊精神、實驗室辦公）</li>
                 <li>學校以及實驗室嚴禁挖礦</li>
             </ul>
             <hr></hr>
-            <h4 className="text-center fw-bold">實驗室參與度之定義</h4>
-            <ul>
-                <li>協助實驗室運作</li>
-                <li>協助國科會計畫</li>
-                <li>增強資訊安全領域相關技能</li>
-                <li>參加資安相關活動</li>
-                <li>知識傳承（協助學弟妹）</li>
-                <li>參與 Group Meeting（每週固定）及個人進度約談（非固定）</li>
-                <li>is1abCTF&#123;is1ab讚!!&#125;</li>
-            </ul>
         </div>
     )
 }
