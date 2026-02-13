f2aad77557cb2f1ab1dc0580e13635884ed3f3bd
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-02-14 01:25:39 +0800

    Update Alert.tsx

--- src/component/Alert.tsx
+++ src/component/Alert.tsx
@@ -1,7 +1,7 @@
 export function Alert(){
     return (
         <div className="p-3 bg-black border border-white border-start-0 border-end-0 border-2 text-center">
-            <p className="fw-bold m-0" style={{color: "yellow"}}>賀！張意昌、吳耀東、林品緯、張睿恩 榮獲 2025 北科實務專題競賽 佳作（實務專題：專案型評測系統設計方法）</p>
+            <p className="fw-bold m-0" style={{color: "yellow"}}>賀！吳冠廷 取得 OSCP 認證</p>
         </div>
     )
 }
