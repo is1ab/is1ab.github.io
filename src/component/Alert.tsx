94e7187f10e3cfa3be93e68e77bc51bad3a454b8
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-02-14 01:33:54 +0800

    Update Alert.tsx

--- src/component/Alert.tsx
+++ src/component/Alert.tsx
@@ -1,7 +1,7 @@
 export function Alert(){
     return (
         <div className="p-3 bg-black border border-white border-start-0 border-end-0 border-2 text-center">
-            <p className="fw-bold m-0" style={{color: "yellow"}}>賀！吳冠廷 取得 OSCP 認證</p>
+            <p className="fw-bold m-0" style={{color: "yellow"}}>賀！吳冠廷 取得 OSCP（Offensive Security Certified Professional）國際資安專業認證</p>
         </div>
     )
 }
