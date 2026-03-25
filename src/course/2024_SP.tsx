90b940b9e6c8027d921504f994c7ace83cec86fb
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-03-25 23:23:05 +0800

    Update 2024_SP.tsx

--- src/course/2024_SP.tsx
+++ src/course/2024_SP.tsx
@@ -13,16 +13,7 @@ export function SP_2024() {
         <li>最高分數: 100 分; 最低分數: 18 分; 平均分數: 89.11 分</li>
         <li>IPAS 證照通過率: 19/22 = 86 %</li>
       </div>
-            
-      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
-        <h5 className="fw-bold text-white m-0">教學評鑑</h5>
-      </div>
-      
-      <div className="py-3 d-flex flex-column gap-3">
-        <li>評量分數: 4.65/5 分</li>
-        <li>填答率: 89 %</li>
-      </div>
-            
+                        
     </div>
   );
 }
