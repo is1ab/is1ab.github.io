109b6477974945377e29dda4c39efdbe17710bf2
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-03-25 20:43:32 +0800

    Add files via upload

---
+++ src/course/secureprogramming.tsx
@@ -0,0 +1,82 @@
+export function SP_2025() {
+  return (
+    <div className="d-flex flex-column gap-3">
+      <h4 className="text-center">113-2 安全程式設計</h4>
+      
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">課程簡介</h5>
+      </div>
+      
+      <div className="py-3 d-flex flex-column gap-3">
+        <p>
+          安全程式設計是資訊安全領域中的關鍵技術。不良的「程式設計習慣」和「不考慮安全保護的程式設計」很容易導致可有心人士所利用。
+          本課程除了建立安全程式設計的基礎知識外，特別著重於同學的實務能力、漏洞測試、與安全程式開發能力。同時，透過分享最新的論文研究與相關技術報告，協助研究生在研究階段獲取最新的領域知識。
+        </p>
+        <p>
+          Security programming is a key technology in the field of information
+          security. “Bad programming habits” and “programming without security
+          consideration” can easily lead to the use of malicious people. Four
+          major themes will be explored.
+        </p>
+        <p>
+          In addition to establishing the basic knowledge of security
+          programming, this course focuses on student's practical ability,
+          vulnerability testing, and security programming development ability.
+          At the same time, by sharing novel research papers and related
+          technical reports, the students can obtain the newest and complete
+          domain knowledge in their research stage.
+        </p>
+      </div>
+      
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">課程內容</h5>
+      </div>
+      
+      <div className="py-3 d-flex flex-column gap-3">
+        <li>軟體及程式安全簡介</li>
+        <li>程式碼安全設計原則</li>
+        <li>SSDLC</li>
+        <li>靜態分析與程式碼審查</li>
+        <li>版本控制</li>
+        <li>GDB</li>
+        <li>字串安全</li>
+        <li>整數安全</li>
+        <li>指標安全</li>
+        <li>加殼技術</li>
+        <li>Testing</li>
+        <li>Fuzzing</li>
+      </div>
+      
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">其他相關教學資源</h5>
+      </div>
+      
+      <div className="py-3 d-flex flex-column gap-3">
+        <li>[Book] Secure Coding: Principles and Practices</li>
+        <li>[Book] Secure Coding in C and C++</li>
+        <li>[Book] Secure Programming Cookbook for C and C++</li>
+      </div>
+
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">學期成績</h5>
+      </div>
+      
+      <div className="py-3 d-flex flex-column gap-3">
+        <li>修課人數: 53 人</li>
+        <li>及格人數: 52 人</li>
+        <li>最高分數: 100 分; 最低分數: 0 分; 平均分數: 87.25 分</li>
+        <li>IPAS 證照通過率: 28/32 = 88 %</li>
+      </div>
+            
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">教學評鑑</h5>
+      </div>
+      
+      <div className="py-3 d-flex flex-column gap-3">
+        <li>評量分數: 4.5/5 分</li>
+        <li>填答率: 83 %</li>
+      </div>
+            
+    </div>
+  );
+}
