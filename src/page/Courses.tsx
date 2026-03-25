079c3f27ddda3a2553e4fc356f1363b464d8ba76
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-03-25 10:40:32 +0800

    Update Courses.tsx

--- src/page/Courses.tsx
+++ src/page/Courses.tsx
@@ -1,69 +1,42 @@
-export function Course() {
-  return (
-    <div className="course">
-      <>
-        <h4 className="text-center fw-bold">113學年 第2學期</h4>
-        <ul>
-          <li>
-            <a href="#/Course/2025/SP">安全程式設計</a>
-          </li>
-          <li>物件導向程式設計實習</li>
-        </ul>
-      </>
+import { ActivityCard } from "../component/ActivityCard"
+import ActivityImage3 from "../assets/activity/activity-image-3.png"
 
-      <>
-        <h4 className="text-center fw-bold">113學年 第1學期</h4>
-        <ul>
-          <li>作業系統 [教學評鑑：4.6/5]</li>
-          <li>物件導向程式設計 [教學評鑑：4.43/5]</li>
-        </ul>
-      </>
-
-      <>
-        <h4 className="text-center fw-bold">112學年 暑假</h4>
-        <ul>
-          <li>程式設計能力強化研習營-Python程式設計</li>
-        </ul>
-      </>
-
-      <>
-        <h4 className="text-center fw-bold">112學年 第2學期</h4>
-        <ul>
-          <li>
-            <a href="#/Course/2024/SP">安全程式設計</a>
-          </li>
-          <li>物件導向程式設計實習 [教學評鑑：4.52/5]</li>
-          <li>實務專題(二) [教學評鑑：N/A]</li>
-          <li>專題討論 [教學評鑑：N/A]</li>
-        </ul>
-      </>
-
-      <>
-        <h4 className="text-center fw-bold">112學年 第1學期</h4>
-        <ul>
-          <li>作業系統 [教學評鑑：4.64/5]</li>
-          <li>物件導向程式設計 [教學評鑑：4.37/5]</li>
-          <li>大學入門與工程倫理 [教學評鑑：N/A]</li>
-          <li>專題討論 [教學評鑑：N/A]</li>
-          <li>實務專題(三) [教學評鑑：N/A]</li>
-        </ul>
-      </>
-
-      <>
-        <h4 className="text-center fw-bold">111學年 第2學期</h4>
-        <ul>
-          <li>安全程式設計（大學部） [教學評鑑：4.68/5]</li>
-          <li>安全程式設計（研究所） [教學評鑑：4.82/5]</li>
-        </ul>
-      </>
-
-      <>
-        <h4 className="text-center fw-bold">111學年 第1學期</h4>
-        <ul>
-          <li>作業系統 [教學評鑑：4.94/5]</li>
-          <li>網路安全 [教學評鑑：4.64/5]</li>
-        </ul>
-      </>
-    </div>
-  );
+export function Activities(){
+    const ActivityItems = [
+        {
+            imgSrc: ActivityImage3,
+            title: "安全程式設計",
+            subTitle: "2025/11/22-2025/11/23 - Tainan City",
+            description: "以「模擬真實企業／國防環境之資安攻防」為導向設計，比賽分為初賽與決賽兩階段。初賽吸引 40 多支學生團隊報名參加。經過初賽後，最終取前 10 組團隊晉級決賽。"
+        },
+        /*{
+            imgSrc: ActivityImage,
+            title: "2024 AIS3 新型態暑期課程",
+            subTitle: "2024/01/01 - Taipei City",
+            description: "以落實產業資安實務為導向設計課程內容，並引導學員依興趣組成跨校跨級學習小組，製作與展示專題成果。錄取考核分為『Pre-exam資安實務測驗』錄取 110 人及『甄選』錄取 40 人，共錄取 150 人。"
+        },
+        {
+            imgSrc: ActivityImage2,
+            title: "2024 資安技能金盾初賽",
+            subTitle: "2024/01/01 - Taipei City",
+            description: "為加強學生資安實務技能，透過競賽機制，以實務與時事題型，提升學生解決資安問題能力與實戰經驗，分為初賽與決賽，參賽對象包含國中、高中及大專校院在學學生，每組最多3人，活動富教育意義且對升學有助益。"
+        },*/
+    ]
+    return (
+        <div className="activities">
+            <h4 className="text-center fw-bold">授課內容</h4>
+            <div className="d-flex flex-column gap-3">
+                {ActivityItems.map((activity) => {
+                    return (
+                        <ActivityCard
+                            imgSrc={activity.imgSrc}
+                            title={activity.title}
+                            subTitle={activity.subTitle}
+                            description={activity.description}
+                        />
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
