4d5b3c2e6c86cecfcd8d152a2412ff5644440db1
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-03-26 00:39:19 +0800

    Update objectorientedprogramming.tsx

--- src/course/objectorientedprogramming.tsx
+++ src/course/objectorientedprogramming.tsx
@@ -1,7 +1,7 @@
-export function SecureProgramming () {
+export function ObjectOrientedProgramming () {
   return (
     <div className="d-flex flex-column gap-3">
-      <h4 className="text-center">安全程式設計 Secure Programming</h4>
+      <h4 className="text-center">物件導向程式設計 Object-Oriented Programming</h4>
       
       <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
         <h5 className="fw-bold text-white m-0">課程簡介</h5>
@@ -9,10 +9,10 @@ export function SecureProgramming () {
       
       <div className="py-3 d-flex flex-column gap-3">
         <p>
-          安全程式設計是資訊安全領域中的關鍵技術。不良的「程式設計習慣」和「不考慮安全保護的程式設計」很容易導致可有心人士所利用。本課程除了建立安全程式設計的基礎知識外，特別著重於同學的實務能力、漏洞測試、與安全程式開發能力。同時，透過分享最新的論文研究與相關技術報告，協助研究生在研究階段獲取最新的領域知識。
+          這門課將提供學生物件導向設計原理的基礎知識，並加強他們在 C++ 程式設計方面的能力。修習這一門課的學生必須要先有具備程式設計的能力，建議先修習計算機程式設計（I）與計算機程式設計（II）。學生將學習如何利用物件導向的角度撰寫設計要求，同時撰寫單元測試以利於保證軟體品質，學生可從單元測試中監控測試覆蓋率，以利於設計測試案例。同時，這一門課將介紹如何使用 Git 進行版本控制，使用 GitLab 和 Jenkins 進行持續整合（CI）。透過理論講解和實作練習，學生將熟悉物件導向設計原則和 C++ 程式設計，並能夠在開發過程中運用單元測試、Git 和 Jenkins 等工具來提升程式品質和效率。
         </p>
         <p>
-          Secure programming is a critical discipline within the field of information security. Poor programming practices and the failure to incorporate security considerations into software design can easily create vulnerabilities that may be exploited by malicious actors. This course not only establishes fundamental knowledge of secure programming, but also places particular emphasis on hands-on skills, vulnerability testing, and the development of secure software. In addition, by introducing the latest research papers and technical reports, the course helps graduate students acquire up-to-date knowledge in this field during their research phase.
+          This course provides students with foundational knowledge of object-oriented design principles and strengthens their proficiency in C++ programming. Students enrolling in this course are expected to have prior programming experience; it is recommended that they complete Computer Programming (I) and Computer Programming (II) beforehand. Students will learn how to formulate design requirements from an object-oriented perspective and develop unit tests to ensure software quality. They will also learn to monitor test coverage through unit testing in order to design effective test cases. In addition, the course introduces the use of Git for version control, as well as GitLab and Jenkins for continuous integration (CI). Through a combination of theoretical instruction and hands-on practice, students will become familiar with object-oriented design principles and C++ programming, and will be able to apply unit testing, Git, and Jenkins in the development process to improve software quality and efficiency.
         </p>
       </div>
       
@@ -21,18 +21,19 @@ export function SecureProgramming () {
       </div>
       
       <div className="py-3 d-flex flex-column gap-3">
-        <li>軟體及程式安全簡介</li>
-        <li>程式碼安全設計原則</li>
-        <li>SSDLC</li>
-        <li>靜態分析與程式碼審查</li>
-        <li>版本控制</li>
-        <li>GDB</li>
-        <li>字串安全</li>
-        <li>整數安全</li>
-        <li>指標安全</li>
-        <li>加殼技術</li>
-        <li>Testing</li>
-        <li>Fuzzing</li>
+        <li>Git Introduction</li>
+        <li>OOP Concept</li>
+        <li>Introducing What C++ is</li>
+        <li>Class / Essential STL Introduction</li>
+        <li>Encapsulation</li>
+        <li>Inheritance</li>
+        <li>Inheritance - Lab</li>
+        <li>Polymorphism</li>
+        <li>Polymorphism - Lab</li>
+        <li>Composition & Interface</li>
+        <li>Composition & Interface - Lab</li>
+        <li>Experience sharing</li>
+
       </div>
       
       <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
@@ -40,9 +41,8 @@ export function SecureProgramming () {
       </div>
       
       <div className="py-3 d-flex flex-column gap-3">
-        <li>[Book] Secure Coding: Principles and Practices</li>
-        <li>[Book] Secure Coding in C and C++</li>
-        <li>[Book] Secure Programming Cookbook for C and C++</li>
+        <li>[Book] Absolute C++ 6/e</li>
+        <li>[PPT] 自編教材</li>
       </div>
 
       <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
@@ -50,10 +50,9 @@ export function SecureProgramming () {
       </div>
       
       <div className="py-3 d-flex flex-column gap-3">
-        <li><a href="https://is1ab.com/#/Course/2025/SP">113-2 (教學評鑑 4.5/5 分，填答率: 83 %)</a></li>
-        <li><a href="https://is1ab.com/#/Course/2024/SP">112-2 (教學評鑑 4.65/5 分，填答率: 89 %)</a></li>
-        <li><a href="https://is1ab.com/#/Course/2023m/SP">111-2 (碩士班) (教學評鑑 4.82/5 分，填答率: 68 %)</a></li>
-        <li><a href="https://is1ab.com/#/Course/2023c/SP">111-2 (大學部) (教學評鑑 4.68/5 分，填答率: 89 %)</a></li>
+        <li><a href="https://is1ab.com/#/Course/2026/OOP">114-1 (教學評鑑 4.41/5 分，填答率: 93 %)</a></li>
+        <li><a href="https://is1ab.com/#/Course/2025/OOP">113-1 (教學評鑑 4.43/5 分，填答率: 94 %)</a></li>
+        <li><a href="https://is1ab.com/#/Course/2024/OOP">112-1 (教學評鑑 4.37/5 分，填答率: 87 %)</a></li>
       </div>
             
     </div>
