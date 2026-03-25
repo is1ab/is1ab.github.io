4e0ec070f4bbd5c9189f9a457fde675d137d1af1
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-03-26 00:54:20 +0800

    Update 2025_OOP.tsx

--- src/course/2025_OOP.tsx
+++ src/course/2025_OOP.tsx
@@ -1,7 +1,7 @@
 export function OOP_2025() {
   return (
     <div className="d-flex flex-column gap-3">
-      <h4 className="text-center">114-1 物件導向程式設計 Object-Oriented Programming</h4>
+      <h4 className="text-center">113-1 物件導向程式設計 Object-Oriented Programming</h4>
     
       <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
         <h5 className="fw-bold text-white m-0">學期成績</h5>
