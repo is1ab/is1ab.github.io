de95e1c6f84d85add6ca209a34c1c66b14d3c1a3
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-03-25 23:14:56 +0800

    Add files via upload

---
+++ src/course/2023_SP.tsx
@@ -0,0 +1,28 @@
+export function SP_2024() {
+  return (
+    <div className="d-flex flex-column gap-3">
+      <h4 className="text-center">112-2 安全程式設計</h4>
+      
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">學期成績</h5>
+      </div>
+      
+      <div className="py-3 d-flex flex-column gap-3">
+        <li>修課人數: 53 人</li>
+        <li>及格人數: 49 人</li>
+        <li>最高分數: 100 分; 最低分數: 18 分; 平均分數: 89.11 分</li>
+        <li>IPAS 證照通過率: 19/22 = 86 %</li>
+      </div>
+            
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">教學評鑑</h5>
+      </div>
+      
+      <div className="py-3 d-flex flex-column gap-3">
+        <li>評量分數: 4.65/5 分</li>
+        <li>填答率: 89 %</li>
+      </div>
+            
+    </div>
+  );
+}
