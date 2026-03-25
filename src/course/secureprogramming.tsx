f30cf10932ccd564af62c5fabbe8c25f73def703
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-03-25 22:39:24 +0800

    Update secureprogramming.tsx

--- src/course/secureprogramming.tsx
+++ src/course/secureprogramming.tsx
@@ -46,11 +46,11 @@ export function SecureProgramming () {
       </div>
 
       <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
-        <h5 className="fw-bold text-white m-0">歷年記錄</h5>
+        <h5 className="fw-bold text-white m-0">歷年開課記錄</h5>
       </div>
       
       <div className="py-3 d-flex flex-column gap-3">
-        <li>修課人數: 53 人</li>
+        <li><a href="https://is1ab.com/#/Course/2025/SP">113-2</a></li>
         <li>及格人數: 52 人</li>
         <li>最高分數: 100 分; 最低分數: 0 分; 平均分數: 87.25 分</li>
         <li>IPAS 證照通過率: 28/32 = 88 %</li>
