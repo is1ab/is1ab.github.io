77cb1a68d00235a20aef4d951feb841a88a49763
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-03-25 23:37:18 +0800

    Update 2023c_SP.tsx

--- src/course/2023c_SP.tsx
+++ src/course/2023c_SP.tsx
@@ -1,16 +1,16 @@
-export function SP_2023m() {
+export function SP_2023c() {
   return (
     <div className="d-flex flex-column gap-3">
-      <h4 className="text-center">111-2 安全程式設計(碩士班)</h4>
+      <h4 className="text-center">111-2 安全程式設計(大學部)</h4>
       
       <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
         <h5 className="fw-bold text-white m-0">學期成績</h5>
       </div>
       
       <div className="py-3 d-flex flex-column gap-3">
-        <li>修課人數: 57 人</li>
-        <li>及格人數: 56 人</li>
-        <li>最高分數: 95 分; 最低分數: 23 分; 平均分數: 85.91 分</li>
+        <li>修課人數: 53 人</li>
+        <li>及格人數: 52 人</li>
+        <li>最高分數: 94 分; 最低分數: 44 分; 平均分數: 85.6 分</li>
         <li>IPAS 證照通過率: * %</li>
       </div>
                         
