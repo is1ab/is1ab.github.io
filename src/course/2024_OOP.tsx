0c3310f1aa26e9bbc916c0da3b163e683a66e244
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-03-26 00:52:04 +0800

    Add files via upload

---
+++ src/course/2024_OOP.tsx
@@ -0,0 +1,19 @@
+export function OOP_2026() {
+  return (
+    <div className="d-flex flex-column gap-3">
+      <h4 className="text-center">114-1 物件導向程式設計 Object-Oriented Programming</h4>
+    
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">學期成績</h5>
+      </div>
+      
+      <div className="py-3 d-flex flex-column gap-3">
+        <li>修課人數: 88 人</li>
+        <li>及格人數: 73 人</li>
+        <li>最高分數: 100 分; 最低分數: 28 分</li>
+        <li>平均分數: 76.07 分</li>
+      </div>
+                  
+    </div>
+  );
+}
