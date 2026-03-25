5220a3714fd69faeb90301d4071f5972a145d538
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-03-26 00:22:46 +0800

    Add files via upload

---
+++ src/course/objectorientedprogramming.tsx
@@ -0,0 +1,61 @@
+export function SecureProgramming () {
+  return (
+    <div className="d-flex flex-column gap-3">
+      <h4 className="text-center">安全程式設計 Secure Programming</h4>
+      
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">課程簡介</h5>
+      </div>
+      
+      <div className="py-3 d-flex flex-column gap-3">
+        <p>
+          安全程式設計是資訊安全領域中的關鍵技術。不良的「程式設計習慣」和「不考慮安全保護的程式設計」很容易導致可有心人士所利用。本課程除了建立安全程式設計的基礎知識外，特別著重於同學的實務能力、漏洞測試、與安全程式開發能力。同時，透過分享最新的論文研究與相關技術報告，協助研究生在研究階段獲取最新的領域知識。
+        </p>
+        <p>
+          Secure programming is a critical discipline within the field of information security. Poor programming practices and the failure to incorporate security considerations into software design can easily create vulnerabilities that may be exploited by malicious actors. This course not only establishes fundamental knowledge of secure programming, but also places particular emphasis on hands-on skills, vulnerability testing, and the development of secure software. In addition, by introducing the latest research papers and technical reports, the course helps graduate students acquire up-to-date knowledge in this field during their research phase.
+        </p>
+      </div>
+      
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">課程內容</h5>
+      </div>
+      
+      <div className="py-3 d-flex flex-column gap-3">
+        <li>軟體及程式安全簡介</li>
+        <li>程式碼安全設計原則</li>
+        <li>SSDLC</li>
+        <li>靜態分析與程式碼審查</li>
+        <li>版本控制</li>
+        <li>GDB</li>
+        <li>字串安全</li>
+        <li>整數安全</li>
+        <li>指標安全</li>
+        <li>加殼技術</li>
+        <li>Testing</li>
+        <li>Fuzzing</li>
+      </div>
+      
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">其他相關教學資源</h5>
+      </div>
+      
+      <div className="py-3 d-flex flex-column gap-3">
+        <li>[Book] Secure Coding: Principles and Practices</li>
+        <li>[Book] Secure Coding in C and C++</li>
+        <li>[Book] Secure Programming Cookbook for C and C++</li>
+      </div>
+
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">歷年開課記錄</h5>
+      </div>
+      
+      <div className="py-3 d-flex flex-column gap-3">
+        <li><a href="https://is1ab.com/#/Course/2025/SP">113-2 (教學評鑑 4.5/5 分，填答率: 83 %)</a></li>
+        <li><a href="https://is1ab.com/#/Course/2024/SP">112-2 (教學評鑑 4.65/5 分，填答率: 89 %)</a></li>
+        <li><a href="https://is1ab.com/#/Course/2023m/SP">111-2 (碩士班) (教學評鑑 4.82/5 分，填答率: 68 %)</a></li>
+        <li><a href="https://is1ab.com/#/Course/2023c/SP">111-2 (大學部) (教學評鑑 4.68/5 分，填答率: 89 %)</a></li>
+      </div>
+            
+    </div>
+  );
+}
