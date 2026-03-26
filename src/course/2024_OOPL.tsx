c6b4dd757abe013aaedbd36b1577864c46526f78
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-03-26 20:59:00 +0800

    Update 2024_OOPL.tsx

--- src/course/2024_OOPL.tsx
+++ src/course/2024_OOPL.tsx
@@ -1,17 +1,17 @@
-export function OOPL_2025() {
+export function OOPL_2024() {
   return (
     <div className="d-flex flex-column gap-3">
-      <h4 className="text-center">113-1 物件導向程式設計實習 Object-Oriented Programming Labs</h4>
+      <h4 className="text-center">112-1 物件導向程式設計實習 Object-Oriented Programming Labs</h4>
     
       <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
         <h5 className="fw-bold text-white m-0">學期成績</h5>
       </div>
       
       <div className="py-3 d-flex flex-column gap-3">
-        <li>修課人數: 87 人</li>
-        <li>及格人數: 84 人</li>
-        <li>最高分數: 100 分; 最低分數: 0 分</li>
-        <li>平均分數: 92.07 分</li>
+        <li>修課人數: 86 人</li>
+        <li>及格人數: 79 人</li>
+        <li>最高分數: 100 分; 最低分數: 2 分</li>
+        <li>平均分數: 86.16 分</li>
       </div>
                   
     </div>
