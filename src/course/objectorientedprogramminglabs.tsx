8ac078bfd206cacd52182ddeaeb53200c5925e7a
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-03-26 20:32:32 +0800

    Add files via upload

---
+++ src/course/objectorientedprogramminglabs.tsx
@@ -0,0 +1,60 @@
+export function ObjectOrientedProgramming () {
+  return (
+    <div className="d-flex flex-column gap-3">
+      <h4 className="text-center">物件導向程式設計 Object-Oriented Programming</h4>
+      
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">課程簡介</h5>
+      </div>
+      
+      <div className="py-3 d-flex flex-column gap-3">
+        <p>
+          這門課將提供學生物件導向設計原理的基礎知識，並加強他們在 C++ 程式設計方面的能力。修習這一門課的學生必須要先有具備程式設計的能力，建議先修習計算機程式設計（I）與計算機程式設計（II）。學生將學習如何利用物件導向的角度撰寫設計要求，同時撰寫單元測試以利於保證軟體品質，學生可從單元測試中監控測試覆蓋率，以利於設計測試案例。同時，這一門課將介紹如何使用 Git 進行版本控制，使用 GitLab 和 Jenkins 進行持續整合（CI）。透過理論講解和實作練習，學生將熟悉物件導向設計原則和 C++ 程式設計，並能夠在開發過程中運用單元測試、Git 和 Jenkins 等工具來提升程式品質和效率。
+        </p>
+        <p>
+          This course provides students with foundational knowledge of object-oriented design principles and strengthens their proficiency in C++ programming. Students enrolling in this course are expected to have prior programming experience; it is recommended that they complete Computer Programming (I) and Computer Programming (II) beforehand. Students will learn how to formulate design requirements from an object-oriented perspective and develop unit tests to ensure software quality. They will also learn to monitor test coverage through unit testing in order to design effective test cases. In addition, the course introduces the use of Git for version control, as well as GitLab and Jenkins for continuous integration (CI). Through a combination of theoretical instruction and hands-on practice, students will become familiar with object-oriented design principles and C++ programming, and will be able to apply unit testing, Git, and Jenkins in the development process to improve software quality and efficiency.
+        </p>
+      </div>
+      
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">課程內容</h5>
+      </div>
+      
+      <div className="py-3 d-flex flex-column gap-3">
+        <li>Git Introduction</li>
+        <li>OOP Concept</li>
+        <li>Introducing What C++ is</li>
+        <li>Class / Essential STL Introduction</li>
+        <li>Encapsulation</li>
+        <li>Inheritance</li>
+        <li>Inheritance - Lab</li>
+        <li>Polymorphism</li>
+        <li>Polymorphism - Lab</li>
+        <li>Composition & Interface</li>
+        <li>Composition & Interface - Lab</li>
+        <li>Experience sharing</li>
+
+      </div>
+      
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">其他相關教學資源</h5>
+      </div>
+      
+      <div className="py-3 d-flex flex-column gap-3">
+        <li>[Book] Absolute C++ 6/e</li>
+        <li>[PPT] 自編教材</li>
+      </div>
+
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">歷年開課記錄</h5>
+      </div>
+      
+      <div className="py-3 d-flex flex-column gap-3">
+        <li><a href="https://is1ab.com/#/Course/2026/OOP">114-1 (教學評鑑 4.41/5 分，填答率: 93 %)</a></li>
+        <li><a href="https://is1ab.com/#/Course/2025/OOP">113-1 (教學評鑑 4.43/5 分，填答率: 94 %)</a></li>
+        <li><a href="https://is1ab.com/#/Course/2024/OOP">112-1 (教學評鑑 4.37/5 分，填答率: 87 %)</a></li>
+      </div>
+            
+    </div>
+  );
+}
