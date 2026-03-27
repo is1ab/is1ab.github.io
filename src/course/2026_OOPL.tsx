feb7cdf9165f660bc2a6e6b493487243688707a1
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-03-27 20:40:46 +0800

    Update 2026_OOPL.tsx

--- src/course/2026_OOPL.tsx
+++ src/course/2026_OOPL.tsx
@@ -4,6 +4,19 @@ export function OOPL_2026() {
       <h4 className="text-center">114-1 物件導向程式設計實習 Object-Oriented Programming Labs</h4>
     
       <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">助教群</h5>
+      </div>
+      
+      <div className="py-3 d-flex flex-column gap-3">
+        <li><a href="mailto:xuan910625.cs13@nycu.edu.tw"> 黃漢軒</a></li>
+        <li><a href="mailto:t111590004@ntut.org.tw"> 張意昌</a></li>
+        <li><a href="mailto:t112820018@ntut.org.tw"> 許景喬</a></li>
+        <li><a href="mailto:t112590022@ntut.org.tw"> 陳駿逸</a></li>
+      </div>
+            
+    </div>
+      
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
         <h5 className="fw-bold text-white m-0">學期成績</h5>
       </div>
       
