82dcd05e21a377db874dec9767b2f20cd0e7a459
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-03-27 19:09:50 +0800

    Update 2025_OOPL.tsx

--- src/course/2025_OOPL.tsx
+++ src/course/2025_OOPL.tsx
@@ -10,8 +10,29 @@ export function OOPL_2025() {
       <div className="py-3 d-flex flex-column gap-3">
         <li>修課人數: 87 人</li>
         <li>及格人數: 84 人</li>
-        <li>最高分數: 100 分; 最低分數: 0 分</li>
+        <li>最高分數: 100 分</li>
+        <li>最低分數: 0 分</li>
         <li>平均分數: 92.07 分</li>
+      </div>
+
+      <div className="p-3 w-100" style={{ backgroundColor: "#7a1b1b" }}>
+        <h5 className="fw-bold text-white m-0">成果展示</h5>
+      </div>
+
+      Rabbit and Steel
+      <div className="w-100">
+        <div className="py-3">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/RVeaaazQheM"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
                   
     </div>
