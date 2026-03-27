fb094c19447efedf44b59257631362a1fd792196
NTUT_VictorYang0623 <125540774+VictorYang0623@users.noreply.github.com>
2026-03-27 08:38:53 +0800

    Update personal page

--- src/member/2025/Victor.tsx
+++ src/member/2025/Victor.tsx
@@ -5,11 +5,24 @@ export const VictorMemberPage = () => {
     const bio = "我是Victor，目前參與sunbird計畫。";
     const certificates: MemberCertificateType[] = [];
     const raceExperiences: MemberCompetitionExperienceType[] = [];
-    const participates: MemberParticipateType[] = [];
+    const participates: MemberParticipateType[] = [
+        {
+            title: "115 幹部研習營 - 築基",
+            datetime: "2026.03.20 - 2026.03.22"
+        }
+    ];
     const internExperiences: MemberInternType[] = [];
     const journals: MemberArticleType[] = [];
     const conferences: MemberArticleType[] = [];
     const techConfs: MemberArticleType[] = [];
+    const presentations: MemberPresentationType[] = [
+        {
+            title: "Nightshade: Prompt-Specific Poisoning Attacks on Text-to-Image Generative Models",
+            authors: "Shawn Shan, Wenxin Ding, Josephine Passananti, Stanley Wu, Haitao Zheng, Ben Y. Zhao",
+            journal: "IEEE Symposium on Security and Privacy (SP)",
+            datetime: "2026.02.05"
+        }
+    ]
 
     return (
         <Sample
