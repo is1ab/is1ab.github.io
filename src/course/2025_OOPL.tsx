3ca8140bf60970fd2ca5bf3c95cf24009ed099df
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-03-28 14:52:14 +0800

    Update 2025_OOPL.tsx

--- src/course/2025_OOPL.tsx
+++ src/course/2025_OOPL.tsx
@@ -99,7 +99,6 @@ export function OOPL_2025() {
         { title: "#1 推箱子", videoId: "laPKN_hNAds" },
         { title: "#2 植物大戰僵屍", videoId: "54DPezZiEIg" },
         { title: "#3 雪人兄弟", videoId: "BZr6w2DVpQE" },
-        // ...
       ];
       
       <div className="row">
