5dbc858a06ba08eb4f8d7fd187d8bb4f8bbf59a8
7his15CYS <127721339+7his15CYS@users.noreply.github.com>
2026-04-02 22:02:17 +0800

    Update Activities.tsx

--- src/page/Activities.tsx
+++ src/page/Activities.tsx
@@ -1,236 +1,261 @@
-import is1ab2025 from "../assets/activities/2025_is1ab_新生盃.png"
-import gold2025 from "../assets/activities/2025_金盾獎.jpg"
-import hackathon2025 from "../assets/activities/2025_AIS3_黑客松.jpg"
-import aegisfinal2025 from "../assets/activities/2025_神盾盃決賽.jpg"
+import is1ab2025 from "../assets/activities/2025_is1ab_新生盃.png";
+import gold2025 from "../assets/activities/2025_金盾獎.jpg";
+import hackathon2025 from "../assets/activities/2025_AIS3_黑客松.jpg";
+import aegisfinal2025 from "../assets/activities/2025_神盾盃決賽.jpg";
 
-export function Activities(){
-    const ActivitiesItem = (props: {
-        time: string
-        descriptionItem: JSX.Element
-    }) => {
-        return (
-            <tr>
-                <td className="border border-1">
-                    {props.time}
-                </td>
-                <td className="border border-1">
-                    {props.descriptionItem}
-                </td>
-            </tr>
-        )
-    }
+type ActivityItemType = {
+  time: string;
+  title: string;
+  notes?: string[];
+  images: string[];
+};
 
-    return (
-        <div className="activities">
-            <h4 className="text-center fw-bold">歷史活動</h4>
-            <table className="w-100 table-bordered">
-                <ActivitiesItem
-                    time="2025.11.23"
-                    descriptionItem={
-                        <div>
-                            <p>2025 神盾盃資安競賽決賽</p>
-                            <img src={aegisfinal2025} width="550"/>
-                        </div>
-                    }
-                ></ActivitiesItem>
-                <ActivitiesItem
-                    time="2025.10.18"
-                    descriptionItem={
-                        <div>
-                            <p>2025 資安技能金盾初賽</p>
-                            <img src={gold2025} width="550"/>
-                        </div>
-                    }
-                ></ActivitiesItem>
-                <ActivitiesItem
-                    time="2025.09.15"
-                    descriptionItem={
-                        <div>
-                            <p>2025 is1ab 新生盃</p>
-                            <img src={is1ab2025} width="550"/>
-                        </div>
-                    }
-                ></ActivitiesItem>
-                <ActivitiesItem
-                    time="2025.08.15"
-                    descriptionItem={
-                        <div>
-                            <p>HITCON CMT 2025</p>
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1AdZbm0KkppfzooqJ2jp-tMBRVeINB-R7&sz=w550" 
-                              />
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1A6JBMKkfNFeQfLssvF5DmFajsfNaVMTK&sz=w550" 
-                              />
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1u_O3Kii-NsOE_6bF19IUiiK3L21L0foO&sz=w550" 
-                              />
-                        </div>
-                    }
-                ></ActivitiesItem>
-                <ActivitiesItem
-                    time="2025.07.28"
-                    descriptionItem={
-                        <div>
-                            <p>2025 AIS3 新型態資安暑期課程</p>
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1nW4NGygr_16Y93etH7l6-mMz8dyfmPMB&sz=w550" 
-                              />
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1d133d1uNoMwPjv2am-L5vdohhj9XuYfs&sz=w550" 
-                              />
-                        </div>
-                    }
-                ></ActivitiesItem>
-                <ActivitiesItem
-                    time="2025.05.03"
-                    descriptionItem={
-                        <div>
-                            <p>2025 AIS3 Club 北區高中職程式安全黑客松工作坊</p>
-                            <img src={hackathon2025} width="550"/>
-                        </div>
-                    }
-                ></ActivitiesItem>
-                <ActivitiesItem
-                    time="2025.02.07"
-                    descriptionItem={
-                        <div>
-                            <p>AIS3 EOF CTF 2025 final</p>
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1xw2zg76QZGzFgUaNnbrb-X_mZ4blyKm2&sz=w550" 
-                              />
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1qmqxe37jXBu-f9Z1j1FiU1s2oatEUgRk&sz=w550" 
-                              />
-                        </div>
-                    }
-                ></ActivitiesItem>
-                <ActivitiesItem
-                    time="2024.11.23"
-                    descriptionItem={
-                        <div>
-                            <p>CTF 種子培訓工作坊 獲金獎</p>
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1PaI1q2ZsJ-ZYb9Bny_qC0wldeI28XipR&sz=w550" 
-                              />
-                        </div>
-                    }
-                ></ActivitiesItem>
-                <ActivitiesItem
-                    time="2024.10.12"
-                    descriptionItem={
-                        <div>
-                            <p>資安技能金盾獎競賽 2024 初賽</p>
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1SgKriQhuqAJcFEwTkiPYwfiHP0lnAukt&sz=w550" 
-                              />
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1k4FmtOMN7ZarHTWcO21Yub3mhFHJIvFJ&sz=w450" 
-                              />
-                        </div>
-                    }
-                ></ActivitiesItem>
-                 <ActivitiesItem
-                    time="2024.04.27"
-                    descriptionItem={
-                        <div>
-                            <p>尋找資安女婕思（決賽）</p>
-                            <p>＊地點：臺南工業局沙崙資安服務基地</p>
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1cCYXbABWN5iwEQAzlUE3T_m4TKTev4C8&sz=w550" 
-                              />
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1nNHcaqCG09UlzrngE_V3EpYwTyZ8rd-U&sz=w450" 
-                              />
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1GmBs6XNTuTnJDPUIyOjZSGo45nLDM7lJ&sz=w550" 
-                              />
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1uf6vptHMCvgUr1XoQrRgIoNNW-8K4loi&sz=w550" 
-                              />
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1yKOkoovnrws5AKuXma7cTLJ1bJyXHpQ4&sz=w550" 
-                              />
-                        </div>
-                    }
-                ></ActivitiesItem>
-                <ActivitiesItem
-                    time="2024.01.21"
-                    descriptionItem={
-                        <div>
-                            <p>實驗室尾牙：旭集和食集錦 SUNRISE</p>
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1vFuyeIm8p5VD4k4cv1sTfhU6X9J7i8lh&sz=w550" 
-                              />
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1tFaOtpgW44YZR_KTg8VIqkAbVSmdLuVx&sz=w550" 
-                              />
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1z9d2b27Myzkm8qCHuvhtOtysF9N5oTzp&sz=w550" 
-                              />
-                        </div>
-                    }
-                ></ActivitiesItem>
-                <ActivitiesItem
-                    time="2023.07.22"
-                    descriptionItem={
-                        <div>
-                            <p>第四屆的奧義學院實習生發表會＆AI 資安社群論壇</p>
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1U-tNyCVfOmSYE04DhK0_N3ldPQRTUwCJ&sz=w550" 
-                              />
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1d9m8BnlOPMjzwVASp8uefOHr2lsiSoOy&sz=w550" 
-                              />
-                        </div>
-                    }
-                ></ActivitiesItem>
-                <ActivitiesItem
-                    time="2023.07.17"
-                    descriptionItem={
-                        <div>
-                            <p> 2023 ICCE-TW 會議</p>
-                            <p>＊Next Generation Cyberseucrity – Session Chair</p>
-                            <p>＊學生論文發表</p>
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1yiEEUdSiLAN3KWsShPzjtmp7QDmSfik7&sz=w450" 
-                              />
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=10oIneb4zj9dSslUC33p902vGavLlZNWc&sz=w450" 
-                              />
-                        </div>
-                    }
-                ></ActivitiesItem>
-                <ActivitiesItem
-                    time="2023.05.06"
-                    descriptionItem={
-                        <div>
-                            <p>2023 尋找資安女婕思（決賽）</p>
-                            <p>＊地點：臺南工業局沙崙資安服務基地</p>
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=15TaJYXuUWz28ksNQIgD4OX33T-Lv_sOn&sz=w550" 
-                              />
-                        </div>
-                    }
-                ></ActivitiesItem>
-                <ActivitiesItem
-                    time="2023.01.21"
-                    descriptionItem={
-                        <div>
-                            <p>實驗室尾牙：豐 Food 海陸百匯</p>
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1K8KTOhV-obkkhd5sZl8q5w2V_Z_2J2Qa&sz=w450" 
-                              />
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=1J8OgQPHjn9lR9XzgtfCQQoJfR6MG-Pk5&sz=w450" 
-                              />
-                            <img 
-                                src="https://drive.google.com/thumbnail?id=15WvGOpe1sUNYyK9zjRgQJaH3Ers1RSnu&sz=w550" 
-                              />
-                        </div>
-                    }
-                ></ActivitiesItem>
-            </table>
+function ActivityCard({
+  item,
+  featured = false,
+}: {
+  item: ActivityItemType;
+  featured?: boolean;
+}) {
+  return (
+    <div
+      className="rounded-4 border p-4"
+      style={{
+        backgroundColor: "#111",
+        borderColor: "rgba(255,255,255,0.10)",
+      }}
+    >
+      <div className="row g-4 align-items-start">
+        <div className="col-12 col-lg-2">
+          <div className="d-flex flex-lg-column align-items-start gap-2">
+            <span
+              className="px-3 py-2 rounded-pill fw-bold"
+              style={{
+                backgroundColor: featured
+                  ? "rgba(255, 77, 79, 0.14)"
+                  : "rgba(255,255,255,0.06)",
+                color: featured ? "#ff6b6b" : "#e5e7eb",
+                border: featured
+                  ? "1px solid rgba(255, 77, 79, 0.28)"
+                  : "1px solid rgba(255,255,255,0.10)",
+                fontSize: "0.92rem",
+                whiteSpace: "nowrap",
+              }}
+            >
+              {featured ? "最新活動" : "活動"}
+            </span>
+
+            <div
+              className="fw-bold"
+              style={{
+                color: "#ffffff",
+                fontSize: "1rem",
+                letterSpacing: "0.01em",
+              }}
+            >
+              {item.time}
+            </div>
+          </div>
         </div>
-    )
+
+        <div className="col-12 col-lg-10">
+          <div className="d-flex flex-column gap-3">
+            <div
+              className="fw-bold"
+              style={{
+                color: "#ffffff",
+                fontSize: "1.35rem",
+                lineHeight: 1.5,
+              }}
+            >
+              {item.title}
+            </div>
+
+            {item.notes && item.notes.length > 0 ? (
+              <div className="d-flex flex-column gap-2">
+                {item.notes.map((note, index) => (
+                  <div
+                    key={index}
+                    className="rounded-3 px-3 py-2"
+                    style={{
+                      backgroundColor: "rgba(56, 189, 248, 0.08)",
+                      border: "1px solid rgba(56, 189, 248, 0.18)",
+                      color: "#7dd3fc",
+                      fontSize: "0.96rem",
+                      lineHeight: 1.8,
+                    }}
+                  >
+                    {note}
+                  </div>
+                ))}
+              </div>
+            ) : null}
+
+            <div className="row g-3">
+              {item.images.map((img, index) => (
+                <div
+                  key={index}
+                  className={item.images.length === 1 ? "col-12" : "col-12 col-md-6"}
+                >
+                  <img
+                    src={img}
+                    alt={`${item.title}-${index + 1}`}
+                    className="img-fluid rounded-4"
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      maxHeight: "520px",
+                      objectFit: "cover",
+                      border: "1px solid rgba(255,255,255,0.10)",
+                    }}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function Activities() {
+  const activities: ActivityItemType[] = [
+    {
+      time: "2025.11.23",
+      title: "2025 神盾盃資安競賽決賽",
+      images: [aegisfinal2025],
+    },
+    {
+      time: "2025.10.18",
+      title: "2025 資安技能金盾初賽",
+      images: [gold2025],
+    },
+    {
+      time: "2025.09.15",
+      title: "2025 is1ab 新生盃",
+      images: [is1ab2025],
+    },
+    {
+      time: "2025.08.15",
+      title: "HITCON CMT 2025",
+      images: [
+        "https://drive.google.com/thumbnail?id=1AdZbm0KkppfzooqJ2jp-tMBRVeINB-R7&sz=w1200",
+        "https://drive.google.com/thumbnail?id=1A6JBMKkfNFeQfLssvF5DmFajsfNaVMTK&sz=w1200",
+        "https://drive.google.com/thumbnail?id=1u_O3Kii-NsOE_6bF19IUiiK3L21L0foO&sz=w1200",
+      ],
+    },
+    {
+      time: "2025.07.28",
+      title: "2025 AIS3 新型態資安暑期課程",
+      images: [
+        "https://drive.google.com/thumbnail?id=1nW4NGygr_16Y93etH7l6-mMz8dyfmPMB&sz=w1200",
+        "https://drive.google.com/thumbnail?id=1d133d1uNoMwPjv2am-L5vdohhj9XuYfs&sz=w1200",
+      ],
+    },
+    {
+      time: "2025.05.03",
+      title: "2025 AIS3 Club 北區高中職程式安全黑客松工作坊",
+      images: [hackathon2025],
+    },
+    {
+      time: "2025.02.07",
+      title: "AIS3 EOF CTF 2025 Final",
+      images: [
+        "https://drive.google.com/thumbnail?id=1xw2zg76QZGzFgUaNnbrb-X_mZ4blyKm2&sz=w1200",
+        "https://drive.google.com/thumbnail?id=1qmqxe37jXBu-f9Z1j1FiU1s2oatEUgRk&sz=w1200",
+      ],
+    },
+    {
+      time: "2024.11.23",
+      title: "CTF 種子培訓工作坊 獲金獎",
+      images: [
+        "https://drive.google.com/thumbnail?id=1PaI1q2ZsJ-ZYb9Bny_qC0wldeI28XipR&sz=w1200",
+      ],
+    },
+    {
+      time: "2024.10.12",
+      title: "資安技能金盾獎競賽 2024 初賽",
+      images: [
+        "https://drive.google.com/thumbnail?id=1SgKriQhuqAJcFEwTkiPYwfiHP0lnAukt&sz=w1200",
+        "https://drive.google.com/thumbnail?id=1k4FmtOMN7ZarHTWcO21Yub3mhFHJIvFJ&sz=w1200",
+      ],
+    },
+    {
+      time: "2024.04.27",
+      title: "尋找資安女婕思（決賽）",
+      notes: ["地點：臺南工業局沙崙資安服務基地"],
+      images: [
+        "https://drive.google.com/thumbnail?id=1cCYXbABWN5iwEQAzlUE3T_m4TKTev4C8&sz=w1200",
+        "https://drive.google.com/thumbnail?id=1nNHcaqCG09UlzrngE_V3EpYwTyZ8rd-U&sz=w1200",
+        "https://drive.google.com/thumbnail?id=1GmBs6XNTuTnJDPUIyOjZSGo45nLDM7lJ&sz=w1200",
+        "https://drive.google.com/thumbnail?id=1uf6vptHMCvgUr1XoQrRgIoNNW-8K4loi&sz=w1200",
+        "https://drive.google.com/thumbnail?id=1yKOkoovnrws5AKuXma7cTLJ1bJyXHpQ4&sz=w1200",
+      ],
+    },
+    {
+      time: "2024.01.21",
+      title: "實驗室尾牙：旭集和食集錦 SUNRISE",
+      images: [
+        "https://drive.google.com/thumbnail?id=1vFuyeIm8p5VD4k4cv1sTfhU6X9J7i8lh&sz=w1200",
+        "https://drive.google.com/thumbnail?id=1tFaOtpgW44YZR_KTg8VIqkAbVSmdLuVx&sz=w1200",
+        "https://drive.google.com/thumbnail?id=1z9d2b27Myzkm8qCHuvhtOtysF9N5oTzp&sz=w1200",
+      ],
+    },
+    {
+      time: "2023.07.22",
+      title: "第四屆的奧義學院實習生發表會＆AI 資安社群論壇",
+      images: [
+        "https://drive.google.com/thumbnail?id=1U-tNyCVfOmSYE04DhK0_N3ldPQRTUwCJ&sz=w1200",
+        "https://drive.google.com/thumbnail?id=1d9m8BnlOPMjzwVASp8uefOHr2lsiSoOy&sz=w1200",
+      ],
+    },
+    {
+      time: "2023.07.17",
+      title: "2023 ICCE-TW 會議",
+      notes: ["Next Generation Cyberseucrity – Session Chair", "學生論文發表"],
+      images: [
+        "https://drive.google.com/thumbnail?id=1yiEEUdSiLAN3KWsShPzjtmp7QDmSfik7&sz=w1200",
+        "https://drive.google.com/thumbnail?id=10oIneb4zj9dSslUC33p902vGavLlZNWc&sz=w1200",
+      ],
+    },
+    {
+      time: "2023.05.06",
+      title: "2023 尋找資安女婕思（決賽）",
+      notes: ["地點：臺南工業局沙崙資安服務基地"],
+      images: [
+        "https://drive.google.com/thumbnail?id=15TaJYXuUWz28ksNQIgD4OX33T-Lv_sOn&sz=w1200",
+      ],
+    },
+    {
+      time: "2023.01.21",
+      title: "實驗室尾牙：豐 Food 海陸百匯",
+      images: [
+        "https://drive.google.com/thumbnail?id=1K8KTOhV-obkkhd5sZl8q5w2V_Z_2J2Qa&sz=w1200",
+        "https://drive.google.com/thumbnail?id=1J8OgQPHjn9lR9XzgtfCQQoJfR6MG-Pk5&sz=w1200",
+        "https://drive.google.com/thumbnail?id=15WvGOpe1sUNYyK9zjRgQJaH3Ers1RSnu&sz=w1200",
+      ],
+    },
+  ];
+
+  return (
+    <div className="container py-4" style={{ maxWidth: "1180px" }}>
+      <div
+        className="p-3 rounded-4 mb-4 text-center"
+        style={{ backgroundColor: "#7a1b1b" }}
+      >
+        <h3 className="fw-bold text-white m-0">歷史活動</h3>
+      </div>
+
+      <div className="d-flex flex-column gap-4">
+        {activities.map((item, index) => (
+          <ActivityCard
+            key={`${item.time}-${item.title}-${index}`}
+            item={item}
+            featured={index < 2}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
