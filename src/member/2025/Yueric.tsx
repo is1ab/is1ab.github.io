2b982e4a4ef91504dbb6558285554bef61e40820
Eric-0522 <wfps4710@gmail.com>
2026-04-14 13:36:07 +0800

    Update personal webpage

--- src/member/2025/Yueric.tsx
+++ src/member/2025/Yueric.tsx
@@ -64,6 +64,11 @@ export const YuericMemberPage = () => {
 			title: "pico CTF 2026",
 			result: "Team Name: NTUT_is1ab, Rank: 261/8747",
 			datetime: "2026/03/10 ~ 2026/03/20",
+		},
+        {
+			title: "Dawg CTF 2026",
+			result: "Team Name: NTUT_is1ab, Rank: 430/1108",
+			datetime: "2026/04/11 ~ 2026/04/13",
 		}
     ]
     const participates: MemberParticipateType[] = [
