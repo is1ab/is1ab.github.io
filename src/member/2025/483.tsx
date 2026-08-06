a431edcf592bfc623bcf3d7c5e87d0ad6ec9cec6
pudding483 <chesterccj483@gmail.com>
2026-08-06 23:52:53 +0800

    Update 483's profile

--- src/member/2025/483.tsx
+++ src/member/2025/483.tsx
@@ -1,174 +1,215 @@
-import { Sample } from "../Sample";
-import _483 from "../../assets/member/483.jpg";
+import { Sample } from '../Sample';
+import _483 from '../../assets/member/483.jpg';
 import {
-	MemberArticleType,
-	MemberCertificateType,
-	MemberInternType,
-	MemberParticipateType,
-	MemberCompetitionExperienceType,
-	MemberPresentationType,
-} from "../../type/MemberCertificateType";
+    MemberArticleType,
+    MemberCertificateType,
+    MemberInternType,
+    MemberParticipateType,
+    MemberCompetitionExperienceType,
+    MemberPresentationType,
+} from '../../type/MemberCertificateType';
 
 export const _483MemberPage = () => {
-	const bio = <>我是沈柏安，你可以稱呼我為 483 。 CTF 比賽 Player Name 通常是 pudding483;<br />
-		Blog : <a href="https://pudding483.github.io/">https://pudding483.github.io/</a></>
-	const certificates: MemberCertificateType[] = [
-		{
-			title: "iPAS 資訊安全工程師-初級能力鑑定",
-			datetime: "2024/12/31",
-		},
-	];
-	const competitionExperiences: MemberCompetitionExperienceType[] = [
-		{
-			title: "TSC CTF 2025",
-			result: "Player Name: pudding483, Rank: 103/509",
-			datetime: "2025/01/15 ~ 2025/01/16",
-		},
-		{
-			title: "pico CTF 2025",
-			result: "Team Name: NTUT Is1ab, Rank: 305/10460",
-			datetime: "2025/03/08 ~ 2025/03/18",
-		},
-		{
-			title: "Cyber Apocalypse CTF 2025: Tales from Eldoria",
-			result: "Team Name: NTUT_Is1ab, Rank: 715/8130",
-			datetime: "2025/03/21 ~ 2025/03/26",
-		},
-		{
-			title: "Swamp CTF 2025",
-			result: "Team Name: NTUT_Is1ab, Rank: 219/751",
-			datetime: "2025/03/29 ~ 2025/03/31",
-		},
-		{
-			title: "Squ1rrel CTF 2025",
-			result: "Team Name: NTUT_Is1ab, Rank: 227/611",
-			datetime: "2025/04/05 ~ 2025/04/07",
-		},
-		{
-			title: "DEFCON CTF 2025",
-			result: "Team Name: 星爆牛炒竹狐, Rank: 31/195",
-			datetime: "2025/04/12 ~ 2025/04/14",
-		},
-		{
-			title: "AIS3 Pre-exam CTF 2025",
-			result: "Player Name: pudding483, Rank: 224/344",
-			datetime: "2025/05/24 ~ 2025/05/26",
-		},
-		{
-			title: "No Hack No CTF 2025",
-			result: "Team Name: NTUT_Is1ab, Rank: 53/473",
-			datetime: "2025/07/05 ~ 2025/07/07",
-		},
-		{
-			title: "DownUnder CTF 2025",
-			result: "Team Name: NTUT_Is1ab, Rank: 508/1667",
-			datetime: "2025/07/18 ~ 2025/07/20",
-		},
-		{
-			title: "WHY CTF 2025",
-			result: "Team Name: NTUT_Is1ab, Rank: 188/1806",
-			datetime: "2025/08/09 ~ 2025/08/11",
-		},
-		{
-			title: "SEKAI CTF 2025",
-			result: "Team Name: NTUT_Is1ab, Rank: 200/1060",
-			datetime: "2025/08/16 ~ 2025/08/18",
-		},
-		{
-			title: "FortID CTF 2025",
-			result: "Team Name: NTUT_Is1ab, Rank: 249/553",
-			datetime: "2025/09/12 ~ 2025/09/14",
-		},
-		{
-			title: "AEGIS 2025 (神盾盃初賽)",
-			result: "Team Name: 在小小的實驗室裡挖阿挖阿挖, Rank: 7/40",
-			datetime: "2025/09/27",
-		},
-		{
-			title: "Patriot CTF 2025",
-			result: "Team Name: Is1ab_Web, Rank: 591/1362",
-			datetime: "2025/11/21 ~ 2025/11/23",
-		},
-		{
-			title: "AIS3 EOF 2025 (初賽)",
-			result: "Team Name: NTUT_ba1si, Rank: 33/82",
-			datetime: "2025/12/20 ~ 2025/12/22",
-		},
-		{
-			title: "Knight CTF 2026",
-			result: "Team Name: NTUT_Is1ab, Rank: 426/890",
-			datetime: "2026/01/15 ~ 2026/01/16",
-		},
-		{
-			title: "Pascal CTF  2026",
-			result: "Team Name: NTUT_Is1ab, Rank: 121/855",
-			datetime: "2026/01/31 ~ 2026/02/01",
-		},
-		{
-			title: "TSC CTF 2026",
-			result: "Player Name: pudding483, Rank: 27/115",
-			datetime: "2026/02/27 ~ 2026/03/01",
-		},
-		{
-			title: "pico CTF 2026",
-			result: "Team Name: NTUT_is1ab, Rank: 261/8747",
-			datetime: "2026/03/10 ~ 2026/03/20",
-		},
-		{
-			title: "TAMU CTF 2026",
-			result: "Team Name: TakeKitsune, Rank: 186/697",
-			datetime: "2026/03/20 ~ 2026/03/22",
-		}
-	];
-	const participates: MemberParticipateType[] = [
-		{
-			title: "SITCON 2025",
-			datetime: "2025/03/08",
-		},
-		{
-			title: "CYBERSEC 2025 臺灣資安大會",
-			datetime: "2025/04/15 ~ 2025/04/17",
-		},
-		{
-			title: "HITCON 2025",
-			datetime: "2025/08/15 ~ 2025/08/16",
-		},
-		{
-			title: "第十屆臺灣好厲駭 高階學員培訓",
-			datetime: "2025/09 ~ now",
-		},
-		{
-			title: "行政院115年網路攻防演練攻擊手遴選",
-			datetime: "2026/03/19",
-		}
-	];
-	const internExperiences: MemberInternType[] = [];
-	const journals: MemberArticleType[] = [];
-	const conferences: MemberArticleType[] = [];
-	const techConfs: MemberArticleType[] = [];
-	const presentations: MemberPresentationType[] = [
-		{
-			title: "TMG-GAN: Generative Adversarial Networks-Based Imbalanced Learning for Network Intrusion Detection",
-			authors: "Hongwei Ding , Yu Sun , Nana Huang, Zhidong Shen , and Xiaohui Cui",
-			journal: "IEEE Transactions on Information Forensics and Security",
-			datetime: "2025/12/18"
-		}
-	];
-	return (
-		<Sample
-			zhName="沈柏安"
-			enName="Shen, Bo-An"
-			avatar={_483}
-			institutes={["國立臺北科技大學 資訊安全碩士學位學程 碩一"]}
-			bio={bio}
-			presentations={presentations}
-			experiences={competitionExperiences}
-			certificates={certificates}
-			participates={participates}
-			intern={internExperiences}
-			journals={journals}
-			conferences={conferences}
-			techConfs={techConfs}
-		></Sample>
-	);
+    const bio = (
+        <>
+            我是沈柏安，你可以稱呼我為 483 。 CTF 比賽 Player Name 通常是
+            pudding483;
+            <br />
+            Blog :{' '}
+            <a href="https://pudding483.github.io/">
+                https://pudding483.github.io/
+            </a>
+        </>
+    );
+    const certificates: MemberCertificateType[] = [
+        {
+            title: 'iPAS 資訊安全工程師-初級能力鑑定',
+            datetime: '2024/12/31',
+        },
+    ];
+    const competitionExperiences: MemberCompetitionExperienceType[] = [
+        {
+            title: 'TSC CTF 2025',
+            result: 'Player Name: pudding483, Rank: 103/509',
+            datetime: '2025/01/15 ~ 2025/01/16',
+        },
+        {
+            title: 'pico CTF 2025',
+            result: 'Team Name: NTUT Is1ab, Rank: 305/10460',
+            datetime: '2025/03/08 ~ 2025/03/18',
+        },
+        {
+            title: 'Cyber Apocalypse CTF 2025: Tales from Eldoria',
+            result: 'Team Name: NTUT_Is1ab, Rank: 715/8130',
+            datetime: '2025/03/21 ~ 2025/03/26',
+        },
+        {
+            title: 'Swamp CTF 2025',
+            result: 'Team Name: NTUT_Is1ab, Rank: 219/751',
+            datetime: '2025/03/29 ~ 2025/03/31',
+        },
+        {
+            title: 'Squ1rrel CTF 2025',
+            result: 'Team Name: NTUT_Is1ab, Rank: 227/611',
+            datetime: '2025/04/05 ~ 2025/04/07',
+        },
+        {
+            title: 'DEFCON CTF 2025',
+            result: 'Team Name: 星爆牛炒竹狐, Rank: 31/195',
+            datetime: '2025/04/12 ~ 2025/04/14',
+        },
+        {
+            title: 'AIS3 Pre-exam CTF 2025',
+            result: 'Player Name: pudding483, Rank: 224/344',
+            datetime: '2025/05/24 ~ 2025/05/26',
+        },
+        {
+            title: 'No Hack No CTF 2025',
+            result: 'Team Name: NTUT_Is1ab, Rank: 53/473',
+            datetime: '2025/07/05 ~ 2025/07/07',
+        },
+        {
+            title: 'DownUnder CTF 2025',
+            result: 'Team Name: NTUT_Is1ab, Rank: 508/1667',
+            datetime: '2025/07/18 ~ 2025/07/20',
+        },
+        {
+            title: 'WHY CTF 2025',
+            result: 'Team Name: NTUT_Is1ab, Rank: 188/1806',
+            datetime: '2025/08/09 ~ 2025/08/11',
+        },
+        {
+            title: 'SEKAI CTF 2025',
+            result: 'Team Name: NTUT_Is1ab, Rank: 200/1060',
+            datetime: '2025/08/16 ~ 2025/08/18',
+        },
+        {
+            title: 'FortID CTF 2025',
+            result: 'Team Name: NTUT_Is1ab, Rank: 249/553',
+            datetime: '2025/09/12 ~ 2025/09/14',
+        },
+        {
+            title: 'AEGIS 2025 (神盾盃初賽)',
+            result: 'Team Name: 在小小的實驗室裡挖阿挖阿挖, Rank: 7/40',
+            datetime: '2025/09/27',
+        },
+        {
+            title: 'Patriot CTF 2025',
+            result: 'Team Name: Is1ab_Web, Rank: 591/1362',
+            datetime: '2025/11/21 ~ 2025/11/23',
+        },
+        {
+            title: 'AIS3 EOF 2025 (初賽)',
+            result: 'Team Name: NTUT_ba1si, Rank: 33/82',
+            datetime: '2025/12/20 ~ 2025/12/22',
+        },
+        {
+            title: 'Knight CTF 2026',
+            result: 'Team Name: NTUT_Is1ab, Rank: 426/890',
+            datetime: '2026/01/15 ~ 2026/01/16',
+        },
+        {
+            title: 'Pascal CTF  2026',
+            result: 'Team Name: NTUT_Is1ab, Rank: 121/855',
+            datetime: '2026/01/31 ~ 2026/02/01',
+        },
+        {
+            title: 'TSC CTF 2026',
+            result: 'Player Name: pudding483, Rank: 27/115',
+            datetime: '2026/02/27 ~ 2026/03/01',
+        },
+        {
+            title: 'pico CTF 2026',
+            result: 'Team Name: NTUT_is1ab, Rank: 261/8747',
+            datetime: '2026/03/10 ~ 2026/03/20',
+        },
+        {
+            title: 'TAMU CTF 2026',
+            result: 'Team Name: TakeKitsune, Rank: 186/697',
+            datetime: '2026/03/20 ~ 2026/03/22',
+        },
+        {
+            title: 'AIS3 MyFirst CTF 2026',
+            result: 'Player Name: pudding483, Rank: 59/329',
+            datetime: '2026/05/16 09:30 ~ 17:30',
+        },
+        {
+            title: 'AIS3 Pre-exam CTF 2026',
+            result: 'Player Name: pudding483, Rank: 70/329',
+            datetime: '2026/05/16 ~ 2026/05/18',
+        },
+        {
+            title: 'SEKAI CTF 2026',
+            result: 'Team Name: NTUT_Is1ab, Rank: 472/928',
+            datetime: '2026/06/27 ~ 2026/06/29',
+        },
+        {
+            title: 'No Hack No CTF 2025',
+            result: 'Team Name: NTUT_Is1ab, Rank: 195/844',
+            datetime: '2026/07/04 ~ 2026/07/06',
+        },
+    ];
+    const participates: MemberParticipateType[] = [
+        {
+            title: 'SITCON 2025',
+            datetime: '2025/03/08',
+        },
+        {
+            title: 'CYBERSEC 2025 臺灣資安大會',
+            datetime: '2025/04/15 ~ 2025/04/17',
+        },
+        {
+            title: 'HITCON 2025',
+            datetime: '2025/08/15 ~ 2025/08/16',
+        },
+        {
+            title: '第十屆臺灣好厲駭 高階學員培訓',
+            datetime: '2025/09 ~ now',
+        },
+        {
+            title: '行政院115年網路攻防演練攻擊手',
+            datetime: '2026/04/13 ~ 2026/09/15',
+        },
+        {
+            title: '115 年度教育體系資安攻防演練 - 攻擊手',
+            datetime: '2026/03/19 ~ 2026/09/17',
+        },
+    ];
+    const internExperiences: MemberInternType[] = [];
+    const journals: MemberArticleType[] = [];
+    const conferences: MemberArticleType[] = [];
+    const techConfs: MemberArticleType[] = [];
+    const presentations: MemberPresentationType[] = [
+        {
+            title: 'TMG-GAN: Generative Adversarial Networks-Based Imbalanced Learning for Network Intrusion Detection',
+            authors:
+                'Hongwei Ding, Yu Sun, Nana Huang, Zhidong Shen ,and Xiaohui Cui',
+            journal: 'IEEE Transactions on Information Forensics and Security',
+            datetime: '2025/12/18',
+        },
+        {
+            title: 'VulnBot: Autonomous Penetration Testing for A Multi-Agent Collaborative Framework',
+            authors:
+                'He Kong, Die Hu, Jingguo Ge, Liangxiong Li, Tong Li,and Bingzhen Wu',
+            journal: 'arXiv : Computer Science > Software Engineering (2025)',
+            datetime: '2026/06/11',
+        },
+    ];
+    return (
+        <Sample
+            zhName="沈柏安"
+            enName="Shen, Bo-An"
+            avatar={_483}
+            institutes={['國立臺北科技大學 資訊安全碩士學位學程 碩一']}
+            bio={bio}
+            presentations={presentations}
+            experiences={competitionExperiences}
+            certificates={certificates}
+            participates={participates}
+            intern={internExperiences}
+            journals={journals}
+            conferences={conferences}
+            techConfs={techConfs}
+        ></Sample>
+    );
 };
